a3cb5eb6681fa2fbb74f3dc278b652a56f0fe604
eilenexuzhe <eilenexuzhe@didiglobal.com>
2022-01-04 18:14:36 +0800

    fix: route-guard

--- components/pkgs/route-guard/route-guard-wrap.tsx
+++ components/pkgs/route-guard/route-guard-wrap.tsx
@@ -1,4 +1,3 @@
-
 import React, { ComponentType, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { dropByCacheKey, useDidCache } from 'react-router-cache-route';
@@ -8,14 +7,14 @@ import { routeType } from './index';
 export interface routeGuardWrapPropsType {
   Component: ComponentType;
   cacheKey?: string;
-  beforeEach?: (props: any) => Promise<Boolean>;
+  beforeEach?: (props: any) => Promise<boolean>;
   switchCacheRouter?: (props: any) => void;
   afterEmit?: (props: any) => void;
   redirect?: string;
-  routeType: routeType
+  routeType: routeType;
 }
 
-export const routeGuardWrap = ({
+export const RouteGuardWrap = ({
   Component,
   cacheKey,
   beforeEach,
@@ -26,7 +25,7 @@ export const routeGuardWrap = ({
 }: routeGuardWrapPropsType) => {
   const RouteGuardWrap = withRouter((props) => {
     const { history } = props;
-    
+
     const before = async () => {
       if (!beforeEach) {
         return;
@@ -35,32 +34,32 @@ export const routeGuardWrap = ({
       const res = await beforeEach(props);
 
       if (res) {
-        history.push(redirect || "");
+        history.push(redirect || '');
         // 卸载组件
-        if(routeType === 'cache') {
+        if (routeType === 'cache') {
           cacheKey && dropByCacheKey(cacheKey);
         }
       }
-    }
+    };
 
     useEffect(() => {
       before();
 
       return () => {
         afterEmit && afterEmit(props);
-      }
+      };
     }, []);
 
     // 监听组件是否被缓存
     useDidCache(() => {
       // 只有 keepalive 才会有路由跳转事件
-      if(routeType === 'cache') {
+      if (routeType === 'cache') {
         switchCacheRouter && switchCacheRouter(props);
       }
     });
 
     return <Component {...props} />;
-  })
+  });
 
   return RouteGuardWrap;
 };
--- components/pkgs/route-guard/route-guard.tsx
+++ components/pkgs/route-guard/route-guard.tsx
@@ -1,50 +1,54 @@
-import React, { useEffect, ComponentType, FC } from 'react';
+import React, { ComponentType, FC } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import CacheRoute, { CacheSwitch, dropByCacheKey } from 'react-router-cache-route';
-import { routeGuardWrap } from './route-guard-wrap';
+import CacheRoute, { CacheSwitch } from 'react-router-cache-route';
+import { RouteGuardWrap } from './route-guard-wrap';
 
 export type routeType = 'default' | 'cache';
 
 export interface routeItemType {
-  path: string,
-  component: ComponentType,
-  cacheKey?: string,
-  redirect?: string
+  path: string;
+  component: ComponentType;
+  cacheKey?: string;
+  redirect?: string;
 }
 
 export interface routePropsType {
   routeList: routeItemType[];
-  beforeEach?: (pathname: string) => Promise<Boolean>;
+  beforeEach?: (pathname: string) => Promise<boolean>;
   switchCacheRouter?: (props: any) => void;
   afterEmit?: (props: any) => void;
   routeType?: routeType;
 }
 
-export const RouteGuard: FC<routePropsType> = ({ routeList, beforeEach, switchCacheRouter, afterEmit, routeType = 'default' }) => {
+export const RouteGuard: FC<routePropsType> = ({
+  routeList,
+  beforeEach,
+  switchCacheRouter,
+  afterEmit,
+  routeType = 'default',
+}: routePropsType) => {
   const AppSwitch = routeType === 'default' ? Switch : CacheSwitch;
-  const AppRoute = routeType === 'default' ? Route as any : CacheRoute;
-  
-  const renderRoute = ({ path, component, cacheKey, redirect }: routeItemType, key: number) => {
-    return <AppRoute
-      path={path}
-      exact={true}
-      component={routeGuardWrap({
-        routeType,
-        Component: component,
-        cacheKey,
-        beforeEach,
-        switchCacheRouter,
-        afterEmit,
-        redirect,
-      })}
-      cacheKey={cacheKey}
-      key={key}
-    />
-  }
+  const AppRoute = routeType === 'default' ? (Route as any) : CacheRoute;
 
-  return <AppSwitch>
-    {
-      routeList.map((item, index) => renderRoute(item, index))
-    }
-  </AppSwitch>
-}
+  const renderRoute = ({ path, component, cacheKey, redirect }: routeItemType, key: number) => {
+    return (
+      <AppRoute
+        path={path}
+        exact={true}
+        component={RouteGuardWrap({
+          routeType,
+          Component: component,
+          cacheKey,
+          beforeEach,
+          switchCacheRouter,
+          afterEmit,
+          redirect,
+        })}
+        cacheKey={cacheKey}
+        key={key}
+      />
+    );
+  };
+
+  return <AppSwitch>{routeList.map((item, index) => renderRoute(item, index))}</AppSwitch>;
+};
