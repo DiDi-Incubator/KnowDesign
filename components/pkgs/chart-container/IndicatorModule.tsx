6f8b27a80aef5e685d2bc9869a7be6d6eafa646a
houxiaolei <houxiaolei@didiglobal.com>
2022-01-28 18:56:44 +0800

    fix: bug

--- components/pkgs/chart-container/IndicatorModule.tsx
+++ components/pkgs/chart-container/IndicatorModule.tsx
@@ -227,7 +227,7 @@ const IndicatorDrawer: React.FC<propsType> = ({
 
   const getAllIndicators = async () => {
     const res: any = await request(requestUrl);
-    const data = res.data || res || [];
+    const data = res || [];
     if (data?.children) {
       if (Array.isArray(data.children)) {
         setTreeDataAll(data.children);
