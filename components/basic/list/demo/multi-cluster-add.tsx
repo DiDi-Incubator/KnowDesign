56c19e49082a8135667b2b7d86163c810d08117e
changjingbin <changjingbin@didiglobal.com>
2022-03-18 17:31:38 +0800

    delete

--- components/basic/list/demo/multi-cluster-add.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from "react";
-import { Drawer } from '@didi/dcloud-design';
-
-const MultiClusterAdd = () => {
-    return <Drawer></Drawer>
-}
-
-export default MultiClusterAdd;
