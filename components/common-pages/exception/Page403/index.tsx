81e38684e5fb9b914fef696a3ae3d9d6abcb341e
guoyuhang <guoyuhang@didiglobal.com>
2022-05-07 16:37:47 +0800

    fix: Page401 403 404 500 组件样式覆盖问题

--- components/common-pages/exception/Page403/index.tsx
+++ components/common-pages/exception/Page403/index.tsx
@@ -4,12 +4,12 @@ import { RouteComponentProps } from 'react-router-dom';
 const Page403: React.FC<RouteComponentProps> = (props) => {
   return (
     <div className="response-tpl-page">
-      <div className="response-tpl-page-content">
+      <div className="response-tpl-page-content page-content-403">
         <div className="response-tpl-page-content-operate">
           <h3 className="response-tpl-page-content-operate-message-sorry">
             抱歉，您没有权限访问该页面
           </h3>
-          <span
+          <div
             className="response-tpl-page-content-operate-message-btn"
             onClick={() => {
               props.history.push({
@@ -18,7 +18,7 @@ const Page403: React.FC<RouteComponentProps> = (props) => {
             }}
           >
             返回首页
-          </span>
+          </div>
         </div>
       </div>
     </div>
